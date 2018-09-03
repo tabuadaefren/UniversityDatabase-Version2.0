92efad18dbb67a66c84a52fc09f4817d97b4e5a0
Efren Tabuada <tabuadaefren@gmail.com>
2018-09-03 15:32:23 +0800

    Designs for Manage-Students modified. Initial commit to Manage-Courses

--- main.py
+++ main.py
@@ -3,27 +3,17 @@ import sqlite3 as sql
 
 app = Flask(__name__)
 
-# CREATE STUDENT OBJECT/STRUCTURE
-class Student(object):
-    def __init__(self, idnum, firstname, lastname, middlename,sex, Course, Yr):
-        self.id_no = idnum
-        self.f_name = firstname
-        self.l_name = lastname
-        self.mid = middlename
-        self.sex = sex
-        self.course = Course
-        self.Yr = Yr
 
 # INITIAL DATABASE CREATION
 conn = sql.connect('database.db')
-conn.execute(
-    '''CREATE TABLE IF NOT EXISTS student(IDNum TEXT PRIMARY KEY  NOT NULL CHECK(length(IDNum)=9), 
-    FName TEXT  CHECK(length(FName)>0 AND length(FName)<=20 ), 
-    LName TEXT CHECK(length(LName)>0 AND length(LName)<=20 ), 
-    MName TEXT CHECK(length(MName)>0 AND length(MName)<=20 ) , 
-    Sex TEXT CHECK(length(Sex)=1), 
-    Course TEXT CHECK(length(Course)>0 AND length(Course)<=20 ), 
-    YrLevel INTEGER CHECK(length(YrLevel)=1))''')
+conn.execute('''CREATE TABLE IF NOT EXISTS student(
+    IDNum TEXT PRIMARY KEY  NOT NULL, 
+    FName TEXT, 
+    MName TEXT,
+    LName TEXT, 
+    Sex TEXT, 
+    Course TEXT, 
+    YrLevel INTEGER)''')
 
 conn.close()
 
@@ -38,7 +28,7 @@ def main():
 # -- Manage Students - main method --
 @app.route("/students",methods = ['POST','GET'])
 def indexStudents():
-    return render_template("index.html")
+    return render_template("index-students.html")
 
 
 
@@ -50,33 +40,26 @@ def add():
 @app.route("/add_submit",methods = ['POST','GET'])
 def add_submit():
     if request.method == "POST":
-        
         try:
-            
             id_number = request.form['ID_Num']
             firstname = request.form['F_Name']
-            lastname = request.form['L_Name']
             middle = request.form['M_Name']
+            lastname = request.form['L_Name']
             sex =request.form['sex']
             course = request.form['course']
             Yr = request.form['Yr_Level']
             
-            stud = Student(id_number,firstname,lastname,middle,sex,course,Yr)
-            print "array"
             with sql.connect("database.db") as conn:
-                print "connect"
                 cur = conn.cursor()
-                cur.execute("INSERT INTO student(IDNum, FName, LName, MName, Sex, Course,YrLevel) VALUES(?,?,?,?,?,?,?)",
-                    (stud.id_no, stud.f_name, stud.l_name, stud.mid, stud.sex, stud.course,stud.Yr))
+                cur.execute("INSERT INTO student(IDNum, FName, MName,  LName, Sex, Course,YrLevel) VALUES(?,?,?,?,?,?,?)",
+                    (id_number,firstname,middle,lastname,sex,course,Yr))
                 conn.commit()
                 msg= "Adding Successful!"
         except:
-        	print " ERROR!"
         	conn.rollback()
         	msg = "Adding failed! "
 
         finally:
-        	print "Final Stage"
         	conn = sql.connect("database.db")
         	conn.row_factory = sql.Row
         	cur = conn.cursor()
@@ -114,44 +97,33 @@ def delete():
 def delete_result():
     if request.method == "POST":
         try:
-            print "entered ID"
             id_number = request.form['ID_Num']
-            print id_number
             with sql.connect("database.db") as conn:
-                print "connected"
                 cur = conn.cursor()
                 cur.execute("SELECT * FROM student")
                 for row in cur.fetchall():
-                    print row
                     if row[0] == id_number:
-                        print "partial in del"
                         cur.execute("DELETE FROM student WHERE IDNum = ?", (id_number,))
-                        print "before commit"
                         conn.commit()
-                        print "committed"
                         msg = "Successfully Deleted"
                         flag=1
                         break
                     else:
-                        print "error delete"
                         flag=0
                         msg = "Error! Student not found."
                         
                         
         except:
             msg = "Fail to delete"
-            print "Failed to delete!"
+            
         finally:
             if flag == 1:
-                print "flag 1"
                 conn = sql.connect("database.db")
                 conn.row_factory = sql.Row
                 cur = conn.cursor()
                 cur.execute("SELECT * FROM student")
                 rows = cur.fetchall()
-                print rows
             else:
-                print "flag 0"
                 rows = " "
             return render_template("add_result.html", rows=rows, msg=msg,)
         conn.close()
@@ -173,14 +145,11 @@ def update_search():
     if request.method == "POST":
         try:
             id_number = request.form['ID_Num']
-            print "meeeee"
             with sql.connect("database.db") as conn:
-                print "connected"
                 cur = conn.cursor()
                 cur.execute("SELECT * FROM student")
                 for row in cur.fetchall():
                     if row[0] == id_number:
-                        print row
                         copied = row
                         msg = " Student Found!"
                         flag = 1
@@ -205,38 +174,25 @@ def update_search():
 @app.route("/update_submit",methods = ['POST', 'GET'])
 def update_submit():
     if request.method =="POST":
-        
         try:
-            print "enter try"
-            
             id_old = request.form['ID_old']
-            id_new = request.form['ID_Num']
-            print id_old
             firstname = request.form['F_Name']
             lastname = request.form['L_Name']
             middle = request.form['M_Name']
             sex = request.form['sex']
             course = request.form['course']
             Yr = request.form['Yr_Level']
-
+            
             with sql.connect("database.db") as conn:
                 cur = conn.cursor()
                 cur.execute("SELECT * FROM student")
                 for row in cur.fetchall():
-                    print row
-
                     if row[0] == id_old:
-                        cur.execute("UPDATE student set FName = ?, LName = ?, MName = ?,  Sex = ?, Course = ?, YrLevel = ? where IDNum = ?",
-                            ( firstname, lastname, middle, sex, course,Yr,id_old))
-                        conn.commit()
-                        cur.execute("UPDATE student set IDNum=? where FName = ? and LName = ?",
-                            (id_new, firstname, lastname))
+                        cur.execute("UPDATE student set FName = ?, LName = ?, MName = ?,  Sex = ?, Course = ?, YrLevel = ? where IDNum = ?",( firstname, lastname, middle, sex, course,Yr,id_old))
                         conn.commit()
                         msg = "successfully UPDATED"
                         break
-                    
         except:
-            print "Fail to update"
             msg = "FAIL to UPDATE"
         finally:
             conn = sql.connect("database.db")
@@ -244,7 +200,7 @@ def update_submit():
             cur = conn.cursor()
             cur.execute("SELECT * FROM student")
             rows = cur.fetchall()
-            return render_template("update_success.html", rows=rows, msg=msg)
+            return render_template("update_success.html", rows=rows, )
             conn.close()
 # -- End of Update Methods --
 
@@ -258,30 +214,25 @@ def search():
 def search_input():
     if request.method == "POST":
         try:
+            print "trial entered"
             count=0
-            SeachKey = request.form['SearchKey']
-            
+            SearchKey = request.form['SearchKey']
+            print SearchKey
             with sql.connect("database.db") as conn:
-                
                 cur = conn.cursor()
-                cur.execute("SELECT * FROM student")
-                for row in cur.fetchall():
-                    if row[0] == id_number:
-                        print row
-                        copied = row
-                        print "search successful"
-                        msg = "Search successful!"
-                        break
-                    else:
-                        print "Error search"
-                        msg = "Error! Student not found."
-                        copied = " "
-
+                print "sql execute"
+                cur.execute("SELECT * FROM student where IDNum = ? or FName = ? or LName=? or MName=?  or Sex=? or YrLevel=? or Course=?", (SearchKey, SearchKey, SearchKey, SearchKey, SearchKey, SearchKey, SearchKey ))
+                print "search database"
+                row = cur.fetchall()
+                print "entered in success"
+                print row
+                msg = "Search successful!"
         except:
-            msg = "ERROR"
-            copied=" "
+            print "entered in failed"
+            msg = "Search failed!"
+            row=" "
         finally:
-            return render_template("search_result.html", msg=msg, copied=copied, )
+            return render_template("search_result.html", msg=msg, row=row, )
             conn.close()
 #---------------------------------------------------
 #End Manage Students
